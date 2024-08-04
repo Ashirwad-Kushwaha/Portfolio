0d97e14f02b8a48d3ee6713af8e94773bcf3bfa5
Ashirwad-Kushwaha <kushwahaashirwad1@gmail.com>
2024-08-04 15:22:05 +0530

    portfolio made

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Web developer with a passion of building innovative and futuristic designs who is quickly adaptable in challenging environment. Experienced with trainings in DSA,  Front-End as well as Back-End web technologies and worked on various projects.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,20 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Ashirwad</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/about-me.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
